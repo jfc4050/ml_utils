fdc8a87f2e14eec366bec140526dc0e60fcab6e9
justin <jfc4050@gmail.com>
2019-01-12 11:12:49 -0800

    fixes in bbox_utils     - slightly faster ijhw_to_ijij     - intermediate conversion to float16 was pointless     - using set operations in NMS

--- ml_utils/bbox_utils.py
+++ ml_utils/bbox_utils.py
@@ -12,9 +12,9 @@ def ijhw_to_ijij(ijhw):
     Returns:
         ndarray: (N, 4); ijij boxes, fractional coordinates.
     """
-    i0j0 = ijhw[:, :2] - ijhw[:, 2:] / 2
-    i1j1 = ijhw[:, :2] + ijhw[:, 2:] / 2
-    ijij = np.concatenate([i0j0, i1j1], axis=1)
+    ij = ijhw[:, :2]
+    hw_rad = ijhw[:, 2:] / 2
+    ijij = np.concatenate([ij - hw_rad, ij + hw_rad], axis=1)
     ijij = np.clip(ijij, 0, 1)  # enforce bounds (relies on fractional coords)
     return ijij
 
@@ -31,8 +31,8 @@ def compute_ious(col_bboxes, row_bboxes):
         ndarray: (nA, nB); iou[a, b] = iou(col_bboxes[a], row_bboxes[b]).
     """
     ### convert: ijhw -> ijij
-    col_ijij = ijhw_to_ijij(col_bboxes).astype('float16')
-    row_ijij = ijhw_to_ijij(row_bboxes).astype('float16')
+    col_ijij = ijhw_to_ijij(col_bboxes)
+    row_ijij = ijhw_to_ijij(row_bboxes)
 
     ### compute intersection
     inter_lt = np.maximum(col_ijij[:, None, :2], row_ijij[:, :2])
@@ -47,38 +47,39 @@ def compute_ious(col_bboxes, row_bboxes):
     ### compute iou
     iou = intersection / union
 
-    return iou.astype('float32')
+    return iou
 
 
-def get_nms_mask(confs, bboxes, nms_iou_thresh=0.5):
+def get_nms_mask(confs, bboxes, iou_thresh=0.5):
     """for each set of bboxes with ious above iou_thresh, eliminates all but
     highest confidence bbox.
 
     Args:
-        confs (ndarray): (N); confidence for each box prediction.
+        confs (ndarray): (N,); confidence for each box prediction.
         bboxes (ndarray): (N, 4); unsuppressed box predictions.
-        nms_iou_thresh (float): minimum iou between two boxes to be considered
+        iou_thresh (float): minimum iou between two boxes to be considered
             an overlap.
 
     Returns:
-        ndarray: (N'): true where prediction should be kept.
+        ndarray: (N,): true where prediction should be kept.
     """
     # inds: box_inds in descending order of confidence
     inds = np.argsort(confs)[::-1]
     ious = compute_ious(bboxes, bboxes)
 
-    inds_to_keep = set(inds)
-    for ind in inds:
-        if ind not in inds_to_keep:  # boxes[ind] already removed
+    kept_inds = set(inds)
+    for i in inds:
+        if i not in kept_inds:  # boxes[i] already removed
             continue
-        # { j | iou(bboxes[i], bboxes[j]) > iou_thresh }
-        overlapping_bbox_inds = np.nonzero(ious[ind, :] > nms_iou_thresh)[0]
-        for overlap_ind in overlapping_bbox_inds:
-            if overlap_ind == ind:  # dont remove self from to_keep
-                continue
-            inds_to_keep.discard(overlap_ind)
+
+        # suppress_inds = { j | iou(bboxes[i], bboxes[j]) > iou_thresh, j != i}
+        suppress_inds, = np.nonzero(ious[i, :] > iou_thresh)
+        suppress_inds = set(suppress_inds)
+        suppress_inds.remove(i)  # dont remove self from inds_to_keep
+
+        kept_inds -= suppress_inds  # perform suppression
 
     mask = np.zeros_like(confs).astype(bool)
-    mask[list(inds_to_keep)] = True
+    mask[list(kept_inds)] = True
 
     return mask
