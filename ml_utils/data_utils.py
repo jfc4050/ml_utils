a108ce8326e8eef6767cadb82ee70c6bc9688489
justin <jfc4050@gmail.com>
2019-03-03 14:18:00 -0800

    returning individual objects instead of aggregating their traits

--- ml_utils/data_utils.py
+++ ml_utils/data_utils.py
@@ -2,14 +2,21 @@
 
 from xml.etree import ElementTree
 from os import PathLike
-from typing import Tuple
+from typing import NamedTuple, Tuple
 
 import numpy as np
 
 
+class PascalObjectLabel(NamedTuple):
+    """pascal label for a single object"""
+    track_id: int
+    class_id: str
+    bbox: Tuple[float]
+
+
 def parse_pascal_xmlfile(
         labelpath: PathLike
-)-> (Tuple[str], Tuple[Tuple[float]]):
+)-> Tuple[PascalObjectLabel]:
     """extract properly formatted labels from xml file.
 
     Args:
@@ -19,8 +26,7 @@ def parse_pascal_xmlfile(
         FileNotFoundError: if no file at labelpath.
 
     Returns:
-        classes: class labels.
-        bboxes: bboxes labels.
+        pascal_objects: tuple of pascal objects
     """
     root = ElementTree.parse(labelpath).getroot()
 
@@ -29,28 +35,22 @@ def parse_pascal_xmlfile(
     im_h, im_w = [int(size.find(key).text) for key in ['height', 'width']]
 
     ### iterate through object labels
-    classes, bboxes = list(), list()
-    for annotation in root.findall('object'):
-        classes.append(annotation.find('name').text)
-
-        bbox = annotation.find('bndbox')
-        bboxes.append([
+    pascal_objects = list()
+    for obj in root.findall('object'):
+        track_id = int(obj.find('trackid').text)
+        class_id = obj.find('name').text
+        bbox = obj.find('bndbox')
+        ijij = np.array([  # ijij, absolute coordinates
             float(bbox.find(key).text)
             for key in ['ymin', 'xmin', 'ymax', 'xmax']
+        ])  # ijhw, absolute coordinates
+        ijhw = np.concatenate([
+            (ijij[:2] + ijij[2:]) / 2,  # ij
+            ijij[2:] - ijij[:2]  # hw
         ])
+        ijhw /= [im_h, im_w, im_h, im_w]  # ijhw, fractional coordinates
+        bbox = tuple(ijhw)
 
-    ### convert bbox coords to ijhw, fractional coords
-    ijij = np.array(bboxes) # ijij, absolute coords
-    ijhw = np.concatenate(
-        [
-            (ijij[:, :2] + ijij[:, 2:]) / 2,  # ij
-            ijij[:, 2:] - ijij[:, :2]  # hw
-        ],
-        axis=1
-    )
-    ijhw /= [im_h, im_w, im_h, im_w]  # absolute -> fractional coords
+        pascal_objects.append(PascalObjectLabel(track_id, class_id, bbox))
 
-    classes = tuple(classes)
-    bboxes = tuple([tuple(row) for row in ijhw])
-
-    return classes, bboxes
+    return tuple(pascal_objects)
